e53fdca9c2367284537d525f545d1576e760242d
Christopher Bodsworth <bodswor2@coventry.ac.uk>
2019-10-31 15:51:23 +0000

    Working on task 3

--- 210Coursework/210Coursework/210Coursework.cpp
+++ 210Coursework/210Coursework/210Coursework.cpp
@@ -10,7 +10,6 @@
 using namespace std;
 
 void ParseFile(vector<tuple<string,int>>* legalWords);
-void CreateBoard(HANDLE console);
 void Play(vector<tuple<string, int>>* legalWords);
 int BoardBinarySearch(vector<tuple<string, int>>* legalWords, string word, int begin, int end);
 void DisplayList(vector<tuple<string, int>>* legalWords);
@@ -18,17 +17,115 @@ void Merge(vector<tuple<string, int>>* legalWords, int begin, int end, int mid);
 void MergeSort(vector<tuple<string, int>>* legalWords, int begin, int end);
 void AssignWordValue(vector<tuple<string, int>>* legalWords);
 
+
 class Tile {
 	public:
+	char tileCharacter;
 	int colourNo;
 	int tileValue;
 	int wordValue;
+	Tile() {
+		tileCharacter = char(254);
+		colourNo = 7;
+		tileValue = 1;
+		wordValue = 1;
+	}
+};
+class Board {
+	public:
+	HANDLE console;
+	Tile tileList[15][15];
+	void FillBoard() 
+	{
+		for (int i = 0; i < 15; i++)
+		{
+			for (int j = 0; j < 15; j++)
+			{
+				Tile tile;
+
+				//double word
+				if (((i == j) || (j == 14 - i)) && ((i != 0) && (i != 5) && (i != 6) && (i != 8) && (i != 9) && (i != 14)))
+				{
+					tile.colourNo = 13;
+					tile.tileValue = 1;
+					tile.wordValue = 2;
+				}
+
+				//double letter
+				else if ((i == 0 && j == 3) || (i == 0 && j == 11)
+					|| (i == 2 && j == 6) || (i == 2 && j == 8)
+					|| (i == 3 && j == 0) || (i == 3 && j == 7) || (i == 3 && j == 14)
+					|| (i == 6 && j == 2) || (i == 6 && j == 6) || (i == 6 && j == 8) || (i == 6 && j == 12)
+					|| (i == 7 && j == 3) || (i == 7 && j == 11)
+					|| (i == 8 && j == 2) || (i == 8 && j == 6) || (i == 8 && j == 8) || (i == 8 && j == 12)
+					|| (i == 11 && j == 0) || (i == 11 && j == 7) || (i == 11 && j == 14)
+					|| (i == 12 && j == 6) || (i == 12 && j == 8)
+					|| (i == 14 && j == 3) || (i == 14 && j == 11))
+				{
+					tile.colourNo = 11;
+					tile.tileValue = 2;
+					tile.wordValue = 1;
+				}
+
+				//triple letter
+				else if ((i == 1 && j == 5) || (i == 1 && j == 9)
+					|| (i == 5 && j == 0) || (i == 5 && j == 5) || (i == 5 && j == 9) || (i == 5 && j == 13)
+					|| (i == 9 && j == 0) || (i == 9 && j == 5) || (i == 9 && j == 9) || (i == 9 && j == 13)
+					|| (i == 13 && j == 5) || (i == 13 && j == 9))
+				{
+					tile.colourNo = 9;
+					tile.tileValue = 3;
+					tile.wordValue = 1;
+				}
+
+				//triple word
+				else if ((i == 0 && j == 0) || (i == 0 && j == 7) || (i == 0 && j == 14) || (i == 7 && j == 0) || (i == 0 && j == 14) || (i == 14 && j == 0) || (i == 14 && j == 7) || (i == 14 && j == 14))
+				{
+					tile.colourNo = 4;
+					tile.tileValue = 1;
+					tile.wordValue = 3;
+				}
+
+				tileList[i][j] = tile;
+			}
+		}
+	};
+	void ShowBoard() 
+	{
+		for (int i = 0; i < 15; i++)
+		{
+			for (int j = 0; j < 15; j++)
+			{
+				SetConsoleTextAttribute(console, tileList[i][j].colourNo);
+				cout << tileList[i][j].tileCharacter << " ";
+			}
+			cout << endl;
+		}
+		SetConsoleTextAttribute(console, 15);
+	}
+	void InsertWord(string word, int xStart, int yStart, char direction)
+	{
+		if (direction == 'H' || direction == 'h') {
+			for (int i = 0; i < word.length(); i++)
+			{
+				tileList[xStart + i][yStart].tileCharacter = word[i];
+			}
+		}
+		else {
+			for (int i = 0; i < word.length(); i++)
+			{
+				tileList[xStart][yStart + i].tileCharacter = word[i];
+			}
+		}
+		system("CLS");
+		ShowBoard();
+	}
+	Board() {
+		console = GetStdHandle(STD_OUTPUT_HANDLE);
+	}
 };
 int main()
 {
-	HANDLE  hConsole;
-	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
 	vector<tuple<string, int>> myWords;
 	ParseFile(&myWords);
 	cout << "Legal word list created" << endl;
@@ -37,23 +134,11 @@ int main()
 	AssignWordValue(&myWords);
 	cout << "Word values assigned" << endl;
 	cout << endl;
-	CreateBoard(hConsole);
+	Board playBoard;
+	playBoard.FillBoard();
+	playBoard.ShowBoard();
 	Play(&myWords);
 }
-
-/*void InsertionSort(vector<tuple<string, int>>* legalWords) 
-{
-	for (int i = 1; i < (*legalWords).size(); i++) 
-	{
-		string key = get<0>((*legalWords)[0]);
-		int j = i-1;
-		while ((j >= 0) && (get<0>((*legalWords)[j]) > key)) 
-		{
-			get<0>((*legalWords)[j + 1]) = get<0>((*legalWords)[j]);
-			j -= 1;
-		}
-	}
-}*/
 
 void DisplayList(vector<tuple<string, int>>* legalWords) 
 {
@@ -90,95 +175,32 @@ void ParseFile(vector<tuple<string, int>>* legalWords)
 	wordFile.close();
 }
 
-void CreateBoard(HANDLE console)
-{
-
-	Tile boardDim[15][15];
-
-	for (int i = 0; i < 15; i++)
-	{
-		for (int j = 0; j < 15; j++)
-		{
-			Tile tile;
-			
-
-			//double word
-			if (( (i == j) || (j == 14 - i) ) && ((i != 0) && (i != 5) && (i != 6) && (i != 8) && (i != 9) && (i != 14)))
-			{
-				tile.colourNo = 13;
-				tile.tileValue = 1;
-				tile.wordValue = 2;
-			}
-
-			//double letter
-			else if ((i == 0 && j == 3) || (i == 0 && j == 11) 
-				|| (i == 2 && j == 6) || (i == 2 && j == 8) 
-				|| (i == 3 && j == 0) || (i == 3 && j == 7) || (i == 3 && j == 14) 
-				|| (i == 6 && j == 2) || (i == 6 && j == 6) || (i == 6 && j == 8) || (i == 6 && j == 12) 
-				|| (i == 7 && j == 3) || (i == 7 && j == 11)
-				|| (i == 8 && j == 2) || (i == 8 && j == 6) || (i == 8 && j == 8) || (i == 8 && j == 12)
-				|| (i == 11 && j == 0) || (i == 11 && j == 7) || (i == 11 && j == 14)
-				|| (i == 12 && j == 6) || (i == 12 && j == 8)
-				|| (i == 14 && j == 3) || (i == 14 && j == 11))
-			{
-				tile.colourNo = 11;
-				tile.tileValue = 2;
-				tile.wordValue = 1;
-			}	
-
-			//triple letter
-			else if ((i == 1 && j == 5) || (i == 1 && j == 9)
-				|| (i == 5 && j == 0) || (i == 5 && j == 5) || (i == 5 && j == 9) || (i == 5 && j == 13)
-				|| (i == 9 && j == 0) || (i == 9 && j == 5) || (i == 9 && j == 9) || (i == 9 && j == 13)
-				|| (i == 13 && j == 5) || (i == 13 && j == 9))
-			{
-				tile.colourNo = 9;
-				tile.tileValue = 3;
-				tile.wordValue = 1;
-			}
-
-			//triple word
-			else if ((i == 0 && j==0) || (i == 0 && j == 7) || (i == 0 && j == 14) || (i == 7 && j == 0) || (i == 0 && j == 14) || (i == 14 && j == 0) || (i == 14 && j == 7) || (i == 14 && j == 14)) 
-			{
-				tile.colourNo = 4;
-				tile.tileValue = 1;
-				tile.wordValue = 3;
-			}
-
-			//generic tile
-			else 
-			{
-				tile.colourNo = 7;
-				tile.tileValue = 1;
-				tile.wordValue = 1;
-			}
-			boardDim[i][j] = tile;
-			SetConsoleTextAttribute(console, tile.colourNo);
-			cout << char(254) << " ";
-		}
-		cout << endl;
-	}
-	SetConsoleTextAttribute(console, 15);
-}
-
 void Play(vector<tuple<string, int>>* legalWords)
 {
 	string playedWord;
 	int wordIndex = -1;
-
+	int xStart;
+	int yStart;
+	char direction;
 	while (wordIndex == -1) 
 	{
 		cout << endl;
 		cout << "What is the word that you want to play?" << endl;
 		cin >> playedWord;
 		wordIndex = BoardBinarySearch(legalWords, playedWord, 0, ((*legalWords).size() - 1));
-		if (wordIndex == -1) 
+		if (wordIndex == -1)
 		{
 			cout << "The given word is illegal" << endl;
 		}
-		
 	}
+
 	cout << "The word is legal and at index " << wordIndex << " in the table" << endl;	
+	cout << "What is the X start position of the word you want to play?" << endl;
+	cin >> xStart;
+	cout << "What is the Y start position of the word you want to play?" << endl;
+	cin >> yStart;
+	cout << "What direction do you want to play your word? H for horizontal and V for vertical." << endl;
+	cin >> direction;
 }
 
 int BoardBinarySearch(vector<tuple<string, int>>* legalWords, string word, int begin, int end) 
